f579942809c23e253849386382de81b6332fdd1a
Clay McLeod <clay.l.mcleod@gmail.com>
2015-04-15 22:32:10 -0500

    Better formatting

--- express-beautiful-ejs-engine.js
+++ express-beautiful-ejs-engine.js
@@ -2,8 +2,8 @@ var ejs = require('ejs');
 var beautify_html = require('js-beautify').html;
 
 module.exports = function (filePath, options, callback) {
- 	var rendered = ejs.__express(filePath, options, function (err, result){
-    if (err) return callback(err, null);
-    return callback(err, beautify_html(result));
+    var rendered = ejs.__express(filePath, options, function (err, result){
+      if (err) return callback(err, null);
+      return callback(err, beautify_html(result));
  	});
 }
