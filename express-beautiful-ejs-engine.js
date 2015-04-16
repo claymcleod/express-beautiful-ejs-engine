861e5885569570834f2bb4e166f64f42ed58a1cb
Clay McLeod <clay.l.mcleod@gmail.com>
2015-04-15 22:31:15 -0500

    First commit

---
+++ express-beautiful-ejs-engine.js
@@ -0,0 +1,9 @@
+var ejs = require('ejs');
+var beautify_html = require('js-beautify').html;
+
+module.exports = function (filePath, options, callback) {
+ 	var rendered = ejs.__express(filePath, options, function (err, result){
+ 		  if (err) return callback(err, null);
+    	return callback(err, beautify_html(result));
+ 	});
+}
